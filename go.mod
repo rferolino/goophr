992f9a829e1e30b79a536f5f766aca63a6c50c6c
Rolly Ferolino <rferolino@jaredsoft.com>
2022-08-02 21:51:43 -0700

    =Added librarian

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/rferolino/goophr
 
 go 1.18
-
-require github.com/rferolino/goophr v0.0.0-20220731072029-aec0de828079
--- librarian/main.go
+++ librarian/main.go
@@ -1,8 +1,20 @@
-// {concierge,librarian}/main.go
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/rferolino/goophr/librarian/api"
+	"github.com/rferolino/goophr/librarian/common"
+)
 
 func main() {
-	fmt.Println("Hello from Librarian!") // Or, Hello from Librarian!
+	common.Log("Adding API handlers...")
+	http.HandleFunc("/api/index", api.IndexHandler)
+	http.HandleFunc("/api/query", api.QueryHandler)
+
+	common.Log("Starting index...")
+	api.StartIndexSystem()
+
+	common.Log("Starting Goophr Librarian server on port :9090...")
+	http.ListenAndServe(":9090", nil)
 }
